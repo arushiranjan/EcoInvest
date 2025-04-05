c6cfe88c887994cc1d8e215b11ecefb5fc22bfd7
Vivek Rai <vkkr1430@gmail.com>
2025-04-05 07:06:27 +0530

    Added sustainable investments

---
+++ investData.js
@@ -0,0 +1,123 @@
+[
+    {
+      "companyName": "Microsoft Corporation",
+      "industry": "Software and Infrastructure",
+      "environmentalScore": 88.50,
+      "socialScore": 70.00,
+      "governanceScore": 72.00,
+      "ESGScore": 76.83,
+      "ESGRiskRating": "A",
+      "industryRank": "1 out of 5",
+      "logo": "https://logos-world.net/wp-content/uploads/2020/11/Microsoft-Logo.png",
+      "symbol": "MSFT"
+    },
+    {
+      "companyName": "Unilever",
+      "industry": "Consumer Goods",
+      "environmentalScore": 85.00,
+      "socialScore": 75.00,
+      "governanceScore": 70.00,
+      "ESGScore": 76.67,
+      "ESGRiskRating": "A",
+      "industryRank": "2 out of 5",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Unilever_logo_2011.svg/1200px-Unilever_logo_2011.svg.png",
+      "symbol": "UL"
+    },
+    {
+      "companyName": "Tesla, Inc.",
+      "industry": "Automobiles",
+      "environmentalScore": 90.00,
+      "socialScore": 65.00,
+      "governanceScore": 68.00,
+      "ESGScore": 74.33,
+      "ESGRiskRating": "B",
+      "industryRank": "3 out of 5",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Tesla_logo.png/1200px-Tesla_logo.png",
+      "symbol": "TSLA"
+    },
+    {
+      "companyName": "Patagonia",
+      "industry": "Apparel",
+      "environmentalScore": 95.00,
+      "socialScore": 80.00,
+      "governanceScore": 75.00,
+      "ESGScore": 83.33,
+      "ESGRiskRating": "A",
+      "industryRank": "1 out of 5",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Patagonia_logo.svg/1200px-Patagonia_logo.svg.png",
+      "symbol": "Private"
+    },
+    {
+      "companyName": "Salesforce",
+      "industry": "Software and Infrastructure",
+      "environmentalScore": 80.00,
+      "socialScore": 78.00,
+      "governanceScore": 74.00,
+      "ESGScore": 77.33,
+      "ESGRiskRating": "A",
+      "industryRank": "2 out of 5",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Salesforce_logo.svg/1200px-Salesforce_logo.svg.png",
+      "symbol": "CRM"
+    },
+    {
+      "companyName": "Google LLC",
+      "industry": "Internet Services",
+      "environmentalScore": 85.00,
+      "socialScore": 72.00,
+      "governanceScore": 70.00,
+      "ESGScore": 75.67,
+      "ESGRiskRating": "A",
+      "industryRank": "3 out of 5",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png",
+      "symbol": "GOOGL"
+    },
+    {
+      "companyName": "Nestlé S.A.",
+      "industry": "Food and Beverage",
+      "environmentalScore": 78.00,
+      "socialScore": 70.00,
+      "governanceScore": 68.00,
+      "ESGScore": 72.00,
+      "ESGRiskRating": "B",
+      "industryRank": "4 out of 5",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Nestlé_logo.svg/1200px-Nestlé_logo.svg.png",
+      "symbol": "NESN.SW"
+    },
+    {
+      "companyName": "IKEA Group",
+      "industry": "Retail",
+      "environmentalScore": 82.00,
+      "socialScore": 68.00,
+      "governanceScore": 66.00,
+      "ESGScore": 72.00,
+      "ESGRiskRating": "B",
+      "industryRank": "5 out of 5",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/IKEA_logo.svg/1200px-IKEA_logo.svg.png",
+      "symbol": "Private"
+    },
+    {
+      "companyName": "Danone S.A.",
+      "industry": "Food and Beverage",
+      "environmentalScore": 80.00,
+      "socialScore": 75.00,
+      "governanceScore": 70.00,
+      "ESGScore": 75.00,
+      "ESGRiskRating": "A",
+      "industryRank": "2 out of 5",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Danone_logo.svg/1200px-Danone_logo.svg.png",
+      "symbol": "BN.PA"
+    },
+    {
+      "companyName": "Apple Inc.",
+      "industry": "Consumer Electronics",
+      "environmentalScore": 89.67,
+      "socialScore": 69.55,
+      "governanceScore": 71.37,
+      "ESGScore": 76.86,
+      "ESGRiskRating": "B",
+      "industryRank": "4 out of 5",
+      "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo.svg/1200px-Apple_logo.svg.png",
+      "symbol": "AAPL"
+    }
+  ]
+  
